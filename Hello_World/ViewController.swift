78aadbe53c899002b3cb025f0b6583817039bac0
hunglyba <tvo001@tvo001s-imac.local>
2016-03-03 10:09:39 +0700

    Initial Commit

---
+++ Hello_World/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hello_World
+//
+//  Created by Tvo001 on 3/3/16.
+//  Copyright © 2016 Tvo001. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
