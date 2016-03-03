93411c74e22c7e0a5fb9d018b5b81972b755b6cb
hunglyba <tvo001@tvo001s-imac.local>
2016-03-03 10:15:35 +0700

    First time commit

--- Hello_World/ViewController.swift
+++ Hello_World/ViewController.swift
@@ -12,7 +12,10 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        // Do any additional setup after loading the view, typically from a nib
+        
+        print("print something here")
+        
     }
 
     override func didReceiveMemoryWarning() {
